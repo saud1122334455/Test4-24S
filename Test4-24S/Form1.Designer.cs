a4e0a97409b548a4dd0e52cb35e71094b71007ad
Saleh860 <saleh_ibraheem@hotmail.com>
2024-05-09 05:27:02 +0300

    Add project files.

---
+++ Test4-24S/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace Test4_24S
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            listBox1 = new ListBox();
+            textBox1 = new TextBox();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // listBox1
+            // 
+            listBox1.Font = new Font("Segoe UI", 14F);
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 25;
+            listBox1.Items.AddRange(new object[] { "Ahmed", "Sami", "Majid", "Ayman", "Asad" });
+            listBox1.Location = new Point(17, 17);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(136, 154);
+            listBox1.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            textBox1.Font = new Font("Segoe UI", 14F);
+            textBox1.Location = new Point(175, 46);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(132, 32);
+            textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Segoe UI", 14F);
+            button1.Location = new Point(197, 97);
+            button1.Name = "button1";
+            button1.Size = new Size(100, 42);
+            button1.TabIndex = 2;
+            button1.Text = "Save";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(329, 191);
+            Controls.Add(button1);
+            Controls.Add(textBox1);
+            Controls.Add(listBox1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private ListBox listBox1;
+        private TextBox textBox1;
+        private Button button1;
+    }
+}
